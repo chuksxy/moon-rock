bb999ea2d9870bea5af70fb3efee1c36a10f1a97
Chuks Abuah <chuksabuah@Chukss-MacBook-Pro.local>
2021-12-22 03:42:47 +0000

    Maybe add comments.

--- game/world/character/Character.cs
+++ game/world/character/Character.cs
@@ -1,19 +1,24 @@
 using game.world.item;
 using UnityEngine;
 
+// This contains all necessary functions related to the Character management and behaviour. 
 namespace game.world.character {
       public static class Character {
+            // Create a character from a `Character Template` ScriptableObject. Give the character an ID and register 
+            // the character within the world.
             public static CharacterInterface Create(
                   TCharacter template,
                   string     characterID,
                   string     registryID,
                   string     zoneID
             ) {
-                  var data = ToData(template);
-                  return Create(data, characterID, registryID, zoneID);
+                  var data = ConvertToData(template);
+                  return Load(data, characterID, registryID, zoneID);
             }
 
-            public static CharacterInterface Create(
+            // Load a character from `Character Data`(Persisted). Assign the ID and register the character within the 
+            // world.
+            public static CharacterInterface Load(
                   CharacterData data,
                   string        characterID,
                   string        registryID,
@@ -22,11 +27,13 @@ namespace game.world.character {
                   return Assemble(data).Init(data, characterID, registryID, zoneID);
             }
 
+            // Assemble a character from a `Character Template` ScriptableObject.
             public static CharacterInterface Assemble(TCharacter template) {
-                  var data = ToData(template);
+                  var data = ConvertToData(template);
                   return Assemble(data);
             }
 
+            // Assemble a character from `Character Data`.
             public static CharacterInterface Assemble(CharacterData characterData) {
                   var character = Load(characterData.Base);
 
@@ -40,10 +47,13 @@ namespace game.world.character {
                   return character.AddComponent<CharacterInterface>();
             }
 
+            // Load the Character's base fbx file from the `Resources` folder by name.
             public static GameObject Load(string name) {
                   return Resources.Load<GameObject>($"/Character/{name}.fbx");
             }
 
+            // Move the character in a specific direction using the CharacterController. Also apply any speed modifier 
+            // that is present.
             internal static void Move(
                   CharacterInterface @interface,
                   Vector3            direction,
@@ -58,6 +68,7 @@ namespace game.world.character {
                   controller.Move(direction * modifier * speed);
             }
 
+            // Jump in the direction specified and apply any jump speed modifier if present.
             internal static void Jump(
                   CharacterInterface @interface,
                   Vector3            direction,
@@ -72,15 +83,18 @@ namespace game.world.character {
                   controller.Move(direction * modifier * speed);
             }
 
+            // Evaluate `Movement Speed` based off the character's current equipment and items in possession.
             private static float EvaluateMovementSpeed(CharacterData characterData) {
                   return 12.0f;
             }
 
+            // Evaluate `Jump Speed` based off the character's current equipment and items in possession.
             private static float EvaluateJumpSpeed(CharacterData characterData) {
                   return 8.0f;
             }
 
-            private static CharacterData ToData(TCharacter template) {
+            // Convert `Character Template` ScriptableObject to a Data object.
+            private static CharacterData ConvertToData(TCharacter template) {
                   return new CharacterData();
             }
       }
