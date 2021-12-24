cb3342b1bb5735500cbb5ff033332392588b5d4a
Chuks Abuah <chuks4abuah@yahoo.com>
2021-12-24 18:09:37 +0000

    property modifier

--- game/world/property/Property.cs
+++ game/world/property/Property.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 namespace game.world.property {
 
       public static partial class Property {
@@ -16,9 +18,10 @@ namespace game.world.property {
 
             public interface IAmAnObject { }
 
-            public interface IModify {
+            public interface IModify<in TInput, out TOutput> {
 
-                  string ID { get; }
+                  string  ID { get; }
+                  TOutput Apply(TInput energy);
 
             }
 
@@ -30,11 +33,68 @@ namespace game.world.property {
 
             }
 
+            public class MaxHealthMultiplier : IModify<Health, Health> {
+
+                  private const string NAME = "multiplier.health.max";
+                  public        string ID => NAME;
+
+                  public float Multiplier { get; set; }
+                  public bool  Restore    { get; set; }
+
+
+                  public Health Apply(Health health) {
+                        if (health.Current <= 0 || health.Max <= 0) {
+                              return health;
+                        }
+
+                        var maxHealth = Mathf.RoundToInt(health.Max * Multiplier);
+                        if (!Restore)
+                              return new Health {
+                                    Max = maxHealth, Current = maxHealth, Modifiers = new string[1] {NAME}
+                              };
+
+                        Restore = false;
+                        return new Health {
+                              Max = maxHealth, Current = health.Current, Modifiers = new string[1] {NAME}
+                        };
+                  }
+
+            }
+
             public class Energy {
 
                   public float    Max       { get; set; }
                   public float    Current   { get; set; }
                   public string[] Modifiers { get; set; }
+
+            }
+
+            public class MaxEnergyMultiplier : IModify<Energy, Energy> {
+
+                  private const string NAME = "multiplier.energy.max";
+
+                  public string ID => NAME;
+
+                  private float Multiplier { get; set; }
+                  private bool  Restore    { get; set; }
+
+
+                  public Energy Apply(Energy energy) {
+                        if (energy.Current <= 0.0f || energy.Max <= 0.0f) {
+                              return energy;
+                        }
+
+                        var maxEnergy = energy.Max * Multiplier;
+                        if (!Restore)
+                              return new Energy {
+                                    Max = maxEnergy, Current = energy.Current, Modifiers = new string[1] {NAME}
+                              };
+
+                        Restore = false;
+                        return new Energy {
+                              Max = maxEnergy, Current = maxEnergy, Modifiers = new string[1] {NAME}
+                        };
+                  }
 
             }
 
