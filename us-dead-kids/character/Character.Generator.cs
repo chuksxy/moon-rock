4eb6f928c4377e5e8f4c05ae9c4e8f0d027060d2
Chuks Abuah <chuks4abuah@yahoo.com>
2022-03-10 14:20:19 +0000

    character generator, work in progress.

--- us-dead-kids/character/Character.Generator.cs
+++ us-dead-kids/character/Character.Generator.cs
@@ -7,35 +7,14 @@ namespace us_dead_kids.character {
             public static class Generator {
 
                   public static Controller Generate(string name, int priority) {
-                        var gameObject = new GameObject();
-
-                        // Setup Character's animation controller at runtime.
-                        bool SetupAnimator() {
-                              var animatorController =
-                                    Resources.Load("master_controller.controller");
-                              if (animatorController == null) {
-                                    Debug.LogWarning("animation controller not found when generating character.");
-                                    return false;
-                              }
-
-                              var animator = gameObject.AddComponent<Animator>();
-                              animator.runtimeAnimatorController = animatorController as RuntimeAnimatorController;
-
-                              return true;
-                        }
-
-                        if (!SetupAnimator()) return null;
-
-                        // Setup character's rigid body as a child game object.
-                        void SetupRigidBody() {
-                              var g = new GameObject();
-                              g.transform.SetParent(gameObject.transform);
-                              g.AddComponent<Rigidbody>();
-                        }
-
-                        SetupRigidBody();
+                        var g = new GameObject();
+                        if (!SetupAnimator(g)) return null;
+                        SetupRigidBody(g);
+                        return SetupController(name, priority, g);
+                  }
 
 
+                  private static Controller SetupController(string name, int priority, GameObject gameObject) {
                         var controller  = gameObject.AddComponent<Controller>();
                         var characterID = $"character_{System.Guid.NewGuid().ToString()}";
                         var character = new Character() {
@@ -44,9 +23,33 @@ namespace us_dead_kids.character {
                               Priority = priority
                         };
                         controller.Init(character);
-
                         return controller;
                   }
+
+
+                  // Setup Character's animation controller at runtime.
+                  private static bool SetupAnimator(GameObject g) {
+                        var animatorController =
+                              Resources.Load("master_controller.controller");
+                        if (animatorController == null) {
+                              Debug.LogWarning("animation controller not found when generating character.");
+                              return false;
+                        }
+
+                        var animator = g.AddComponent<Animator>();
+                        animator.runtimeAnimatorController = animatorController as RuntimeAnimatorController;
+
+                        return true;
+                  }
+
+
+                  // Setup character's rigid body as a child game object.
+                  private static void SetupRigidBody(GameObject parent) {
+                        var g = new GameObject();
+                        g.transform.SetParent(parent.transform);
+                        g.AddComponent<Rigidbody>();
+                  }
+
 
             }
 
